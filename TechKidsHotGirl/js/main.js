9d63f203701786a8290572f5c5fac147b9741f9a
AVAVT <itsavavt@gmail.com>
2017-01-08 20:20:09 +0700

    FE5 Homework

--- TechKidsHotGirl/js/main.js
+++ TechKidsHotGirl/js/main.js
@@ -1,11 +1,15 @@
-$(document).ready(function(){
+var loadedData = [];
 
+$(document).ready(function(){
   var itemTemplate = Handlebars.compile($("#item-template").html());
+  var itemModalTemplate = Handlebars.compile($("#item-modal-template").html());
 
   $.ajax({
     type  : "get",
     url   : "imagesData.json"
   }).then(function(data){
+    loadedData = loadedData.concat(data.items);
+
     var itemHtml = itemTemplate(data);
     $("#item_list").html(itemHtml);
     $('#item_list').masonry({
@@ -16,4 +20,20 @@ $(document).ready(function(){
   }).fail(function(error){
     console.log(error);
   });
+
+  $('body').on('click', '.plus_button', function(){
+    var itemId = $(this).attr('data-item-id');
+
+    for(var i=0; i< loadedData.length; i++){
+      var itemData = loadedData[i];
+      if(itemData.id == itemId){
+        // Found item, populating data
+        $("#item_modal_body").html(itemModalTemplate(itemData));
+        break;
+      }
+    }
+
+    $("#item_modal").modal("show");
+  });
+
 });
