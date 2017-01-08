4c990fbd2929f78d871cea75e0863563df9447e8
AVAVT <itsavavt@gmail.com>
2017-01-08 20:49:09 +0700

    FE6

--- TechKidsHotGirl/js/main.js
+++ TechKidsHotGirl/js/main.js
@@ -4,22 +4,7 @@ $(document).ready(function(){
   var itemTemplate = Handlebars.compile($("#item-template").html());
   var itemModalTemplate = Handlebars.compile($("#item-modal-template").html());
 
-  $.ajax({
-    type  : "get",
-    url   : "imagesData.json"
-  }).then(function(data){
-    loadedData = loadedData.concat(data.items);
-
-    var itemHtml = itemTemplate(data);
-    $("#item_list").html(itemHtml);
-    $('#item_list').masonry({
-      itemSelector: '.item_container',
-      columnWidth: '.item_container',
-      percentPosition: true
-    });
-  }).fail(function(error){
-    console.log(error);
-  });
+  requestNextPage(itemTemplate);
 
   $('body').on('click', '.plus_button', function(){
     var itemId = $(this).attr('data-item-id');
@@ -36,4 +21,28 @@ $(document).ready(function(){
     $("#item_modal").modal("show");
   });
 
+  $(window).on('scroll', function(){
+    if($(window).scrollTop() + window.innerHeight > $(document).height() - 200){
+      requestNextPage(itemTemplate);
+    }
+  });
 });
+
+function requestNextPage(itemTemplate){
+  $.ajax({
+    type  : "get",
+    url   : "imagesData.json"
+  }).then(function(data){
+    loadedData = loadedData.concat(data.items);
+
+    var itemHtml = itemTemplate(data);
+    $("#item_list").append(itemHtml);
+    // $('#item_list').masonry({
+    //   itemSelector: '.item_container',
+    //   columnWidth: '.item_container',
+    //   percentPosition: true
+    // });
+  }).fail(function(error){
+    console.log(error);
+  });
+}
